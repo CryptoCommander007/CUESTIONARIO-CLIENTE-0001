4c6199d02528261008eaf78acedf35afaa0f70b2
CryptoCommander007 <rivantped@gmail.com>
2024-06-19 05:37:33 -0400

    CUESTIONARIO-CLIENTE-0001

---
+++ run_commands.bat
@@ -0,0 +1,6 @@
+git init
+git add .
+git commit -m "CUESTIONARIO-CLIENTE-0001"
+git branch -M main
+git remote add origin https://github.com/CryptoCommander007/CUESTIONARIO-CLIENTE-0001.git
+git push -u origin main
